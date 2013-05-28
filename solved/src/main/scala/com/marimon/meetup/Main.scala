6413cae0ad8c7a96cef22c3ff38404829e45bc02
Ignasi Marimon-Clos i Sunyol <ignasi35@gmail.com>
2013-05-28 23:46:27 +0200

    implemented a POST

--- solved/src/main/scala/com/marimon/meetup/Main.scala
+++ solved/src/main/scala/com/marimon/meetup/Main.scala
@@ -11,8 +11,23 @@ import com.marimon.meetup.providers.Group
 import com.marimon.meetup.providers.Person
 import com.marimon.meetup.providers.Song
 import com.marimon.meetup.providers.Book
+import spray.http.MultipartFormData
+import spray.httpx.marshalling.MultipartMarshallers
+import spray.http.MultipartContent
+import com.marimon.meetup.providers.CDDVD
 
 object Main extends App with SimpleRoutingApp {
+
+  val u2 = Group("U2", Some(List(Person("Bono"), Person("TheEdge"))))
+  val gorillaz = Group("Gorillaz", None)
+
+  var stock =
+    List(CDDVD(u2,
+      "Joshua Tree",
+      List(Song("Where the streets have no name", 338), Song("With or without you", 296))),
+      CDDVD(gorillaz,
+        "Demon Days",
+        List(Song("Intro", 63), Song("Feel Good Inc.", 221))))
 
   startServer(interface = "localhost", port = 8080) {
 
@@ -35,18 +50,24 @@ object Main extends App with SimpleRoutingApp {
         get{
           complete{
             import com.marimon.meetup.providers.MyBookStoreProtocol._
-            val u2 = Group("U2", Some(List(Person("Bono"), Person("TheEdge"))))
-            val gorillaz = Group("Gorillaz", None)
-
-            List(CDDVD(u2,
-              "Joshua Tree",
-              List(Song("track1", 165), Song("track 2", 230))),
-              CDDVD(gorillaz,
-                "Clint Eastwood",
-                List(Song("track3 ", 165), Song("track 4", 230))))
+            stock
 
           }
-        }
+        } ~
+          post{
+            // invoke using curl -X POST -d @book-sample.json http://localhost:8080/books.json
+            // use book-sample.json from src/main/resources
+            ctx =>
+              ctx.complete{
+                // TODO: use proper encoding
+                val input = new String(ctx.request.entity.buffer)
+                import spray.json._
+                val x = input.asJson
+                import com.marimon.meetup.providers.MyBookStoreProtocol._
+                stock = x.convertTo[CDDVD[Group]] :: stock
+                "added: " + stock.head
+              }
+          }
       }
 
   }
